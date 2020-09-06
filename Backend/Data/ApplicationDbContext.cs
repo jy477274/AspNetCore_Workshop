2fed60d051714d72f7335b5c5a6e28a700c3fc98
jy477274 <jayden_macdonald@outlook.com>
2020-09-06 14:26:47 -0300

    Adding initial backend files from modules 1 and 2

---
+++ Backend/Data/ApplicationDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Attendee>().HasIndex(x => x.UserName).IsUnique();
+
+            //Many to Many: Session <-> Attendee
+            modelBuilder.Entity<SessionAttendee>().HasKey(sa => new { sa.SessionId, sa.AttendeeId });
+
+            //Many to Many: Speaker <-> Session
+            modelBuilder.Entity<SessionSpeaker>().HasKey(ss => new { ss.SessionId, ss.SpeakerId });
+        }
+
+        public DbSet<Speaker> Speakers { get; set; }
+
+        public DbSet<Session> Sessions { get; set; }
+
+        public DbSet<Attendee> Attendees { get; set; }
+
+        public DbSet<Track> Tracks { get; set; }
+    }
+}
---
+++ Backend/Data/EntityExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Backend.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Infrastructure
+{
+    public static class EntityExtensions
+    {
+        public static ConferenceDTO.SpeakerResponse MapSpeakerResponse(this Speaker speaker) =>
+            new ConferenceDTO.SpeakerResponse
+            {
+                Id = speaker.Id,
+                Name = speaker.Name,
+                Bio = speaker.Bio,
+                Website = speaker.Website,
+
+                Sessions = speaker.SessionSpeakers?
+                    .Select(ss => new ConferenceDTO.Session
+                    {
+                        Id = ss.SessionId,
+                        Title = ss.Session.Title
+                    }).ToList()
+            };
+    }
+}
