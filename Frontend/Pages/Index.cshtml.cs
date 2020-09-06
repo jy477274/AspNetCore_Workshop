f2c31c125cbcf9bdc5633c02c355625e072e7465
jy477274 <jayden_macdonald@outlook.com>
2020-09-06 17:17:57 -0300

    Adding initial Front End Code

---
+++ Frontend/Pages/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Frontend.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace Frontend.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        protected readonly IApiClient _apiClient;
+
+        public IndexModel(ILogger<IndexModel> logger, IApiClient apiClient)
+        {
+            _logger = logger;
+            _apiClient = apiClient;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
